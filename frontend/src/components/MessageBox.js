e6b67acb64f624cb5f7b342d95346376530e83b2
Angel Camargo <adcamargo10@gmail.com>
2023-02-12 15:12:34 -0400

    Deploying Glitter App

---
+++ frontend/src/components/MessageBox.js
@@ -0,0 +1,7 @@
+import Alert from "react-bootstrap/Alert";
+
+export default function MessageBox(props) {
+    return (
+        <Alert variant={props.variant || 'Info'}>{props.children}</Alert>
+    );
+}
